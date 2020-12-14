f73d56b12d8df07b5036862e1815f5b7d14e1b10
Temperton, Zak Barry <zakbarrytemperton@gmail.com>
2020-12-14 18:02:54 +1300

    reordered parts/mod.rs

--- src/parts/mod.rs
+++ src/parts/mod.rs
@@ -1,11 +1,14 @@
 mod plugboard;
+pub use plugboard::Plugboard;
+
 mod reflector;
+pub use reflector::{Reflector, ReflectorCode};
+
 mod rotor;
+pub use rotor::{Rotor, RotorCode};
+
 mod rotors;
+pub use rotors::Rotors;
 
 pub const ALPHABET_LEN: usize = 26;
 
-pub use plugboard::Plugboard;
-pub use reflector::{Reflector, ReflectorCode};
-pub use rotor::{Rotor, RotorCode};
-pub use rotors::Rotors;
