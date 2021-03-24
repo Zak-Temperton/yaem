691eb41c5ee9c6e30c58ad516b10b34639a71456
Temperton, Zak Barry <zakbarrytemperton@gmail.com>
2021-03-24 17:47:23 +1300

    some small polish

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,20 @@ use parts::{
 };
 
 fn main() {
-  let rotor1 = Rotor::new(RotorCode::I).with_rotation(5);
-  let rotor2 = Rotor::new(RotorCode::II).with_rotation(1);
-  let rotor3 = Rotor::new(RotorCode::III).with_rotation(24);
-  let reflector = Reflector::new(ReflectorCode::ReflectorA);
-  let rotors = Rotors::new(rotor1, rotor2, rotor3, reflector);
-  let plugboard = Plugboard::new()
-    .add_connection(1, 3)
-    .add_connection(25, 7)
-    .add_connection(2, 4)
-    .add_connection_from_chars('f', 'j')
-    .build();
-  let mut emach_1 = EnigmaMachine::new(rotors, plugboard);
+  let mut emach_1 = {
+    let rotor1 = Rotor::new(RotorCode::I).with_rotation(5);
+    let rotor2 = Rotor::new(RotorCode::II).with_rotation(1);
+    let rotor3 = Rotor::new(RotorCode::III).with_rotation(24);
+    let reflector = Reflector::new(ReflectorCode::ReflectorA);
+    let rotors = Rotors::new(rotor1, rotor2, rotor3, reflector);
+    let plugboard = Plugboard::new()
+      .add_connection(1, 3)
+      .add_connection(25, 7)
+      .add_connection(2, 4)
+      .add_connection_from_chars('f', 'j')
+      .build();
+    EnigmaMachine::new(rotors, plugboard)
+  };
   let mut emach_2 = emach_1.clone();
 
   let args = env::args();
@@ -31,12 +33,11 @@ fn main() {
     return;
   }
 
-  let input = args.skip(1).next().unwrap(); //skip ffirst as it is the name of the program
+  let input = args.skip(1).next().unwrap(); //skip first arg as it is the program name
   let mut result = String::with_capacity(input.len());
   for i in input.bytes() {
     let i = u8_to_usize(i);
     assert!(i < ALPHABET_LEN);
-
     let encoded = emach_1.pass(i);
     let decoded = emach_2.pass(encoded);
 
@@ -46,10 +47,10 @@ fn main() {
       usize_to_char(i),
       usize_to_char(encoded),
       usize_to_char(decoded),
-      i == decoded
+      i == decoded,
     );
   }
-  println!("\nEncoded: {}", result.as_str());
+  println!("\nEncoded: {}", result);
 }
 
 #[inline(always)]
